a0608eece2dc63f55a967621b31a739070c5c3d0
yojana29 <yojanapandey29@gmail.com>
2025-08-06 22:43:22 +0545

    first commit

---
+++ day2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrintNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter 'odd' or 'even': ");
+            string type = Console.ReadLine();
+            Console.Write("How many number do you want to print?");
+            int n = int.Parse(Console.ReadLine());
+            int start = (type == "even") ? 2 : 1;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(start + i * 2 + " ");
+
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ day3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Operations
+{
+    class Addition
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first numbers: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second Number: ");
+            int y = int.Parse(Console.ReadLine());
+
+            int sum = x + y;
+            Console.WriteLine($"The Sum of {x} and {y} is {sum}");
+
+            int subtract = x - y;
+            Console.WriteLine($"The difference of {x} and {y} is {subtract}");
+
+            int mul = x * y;
+            Console.WriteLine($"The multiplication of {x} and {y} is {mul}");
+
+            int div = x / y;
+            Console.WriteLine($"The division of {x} and {y} is {div}");
+
+
+        }
+    }
+}
