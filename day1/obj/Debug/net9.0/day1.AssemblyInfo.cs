f07266c902ba02eceb1cd5d6cc7f795fcab9a1a3
yojana29 <yojanapandey29@gmail.com>
2025-08-08 10:03:55 +0545

    constructor

--- day1/obj/Debug/net9.0/day1.AssemblyInfo.cs
+++ day1/obj/Debug/net9.0/day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0608eece2dc63f55a967621b31a739070c5c3d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ day4Constructor/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+//parameteriszed constructor
+namespace ClassObject
+{
+    class Details
+    {
+        string Name;
+        int Age;
+        public Details(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+        }
+
+    }
+
+    // class Program
+    // {
+    //     static void Main(string[] args)
+    //     {
+    //         Details s1 = new Details("Ram", 20);
+    //         s1.ShowInfo();
+
+    //     }
+    // }
+
+    namespace DefaultConstructor
+    {
+        class Information
+        {
+            private string StdName;
+            private int RollNo;
+
+            public Information()
+            {
+                StdName = "Hari";
+                RollNo = 1234;
+            }
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Name: {StdName} and RollNo: {RollNo} ");
+            }
+        }
+
+        class SecondProgram
+        {
+            static void Main()
+            {
+                Information I1 = new Information();
+                I1.DisplayInfo();
+            }
+        }
+    }
+}
