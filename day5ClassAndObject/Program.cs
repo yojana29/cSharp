fc8b4f1535b2e84d3472251b136b0b89b8b2953b
yojana29 <yojanapandey29@gmail.com>
2025-08-09 23:29:52 +0545

    class and object

---
+++ day5ClassAndObject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ClassExample
+{
+    class Student
+    {
+        public string name;
+        public string address;
+        public int age;
+        public int rollNo;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Address:" + address);
+            Console.WriteLine("Age:" + age);
+            Console.WriteLine("RollNo:" + rollNo);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.name = "RAM";
+            s1.address = "MNR";
+            s1.age = 20;
+            s1.rollNo = 54;
+            s1.ShowInfo();
+        }
+      
+    }
+}
