a820cb30c0f085091edac7aebb35c652db90883a
yojana29 <yojanapandey29@gmail.com>
2025-08-11 19:42:07 +0545

    completed

---
+++ day6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Student
+{
+    private string name;   // private field
+
+    // Public property
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Name: " + name);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s1 = new Student();
+
+        // Set value using property
+        s1.Name = "Sita";
+
+        // Get value using method
+        s1.Show();
+    }
+}
