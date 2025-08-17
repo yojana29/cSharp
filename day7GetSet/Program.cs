efd5606869a12c90bcb01caffe13caa4b3fedd37
yojana29 <yojanapandey29@gmail.com>
2025-08-17 18:24:56 +0545

    completed

---
+++ day7GetSet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+    class Example
+    {
+        public string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name:" + name);
+        }
+    }
+class Program
+{
+    static void Main()
+    {
+        Example E1 = new Example();
+        E1.Name = "RAM";
+        E1.ShowInfo();
+    }
+        
+    }
+
