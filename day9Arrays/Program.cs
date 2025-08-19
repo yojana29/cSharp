32e0fda8ef18bc5a3a0731fd92dd56e11853910c
yojana29 <yojanapandey29@gmail.com>
2025-08-19 21:57:48 +0545

    added

---
+++ day9Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Arrays
+{
+    class ArrayExample
+    {
+        static void Main()
+        {
+            int[] numbers = {10, 20, 30, 40, 50 };
+
+            //printing element using index
+            Console.WriteLine("/First Element" + numbers[0]);
+
+            //printing all elements
+            for (int i = 0; i < numbers.Length; i++) {
+                Console.WriteLine($"{i} element" + numbers[i]);
+            }
+        }
+    }
+}
