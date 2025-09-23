c0deb3ff186bfadef6e2aa1df5da1d3454088fc3
yojana29 <yojanapandey29@gmail.com>
2025-09-23 21:34:05 +0545

    basekeyword

---
+++ day13BaseKeyword/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Person
+{
+    public void Hello()
+    {
+        Console.WriteLine(" My name is Ram.");
+    }
+
+}
+class Greeting : Person
+{
+    public void Hello()//hides the base method
+    {
+        Console.WriteLine(" Hello! Everyone.");
+    }
+    public void ShowInfo()
+    {
+        Hello();//calls greeting one 
+         base.Hello();//calls person one
+
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Greeting g = new Greeting();
+        g.ShowInfo();
+    }
+}
