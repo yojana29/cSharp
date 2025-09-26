4bae801a100a570523917b067e4e359b35375436
yojana29 <yojanapandey29@gmail.com>
2025-09-26 09:25:31 +0545

    basefor variable

---
+++ day14BaseKeyword/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Person//base class
+{
+    public string Name = "Base: Ram";
+}
+class Student : Person //derived class
+{
+    public string Name = "Derived: Hari";
+
+    public void showDetail()
+    {
+        Console.WriteLine(Name);//refers to name of derived class
+        Console.WriteLine(base.Name);//refers to name base class
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student();
+        s.showDetail();
+    }
+}
