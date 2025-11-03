078299806b916ae4fd5bfed9b5802fd2b59a3cd2
yojana29 <yojanapandey29@gmail.com>
2025-11-03 21:49:30 +0545

    delegate

---
+++ delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Step 1: Declare a delegate
+delegate void GreetingDelegate(string name);
+
+class Program
+{
+    // Step 2: Create a method matching delegate’s pattern
+    static void SayHello(string name)
+    {
+        Console.WriteLine("Hello " + name);
+    }
+
+    static void Main()
+    {
+        // Step 3: Create delegate object and point to method
+        GreetingDelegate greet = SayHello;
+
+        // Step 4: Call the delegate (which calls the method)
+        greet("Ram");
+    }
+}
